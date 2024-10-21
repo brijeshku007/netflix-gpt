ea5c33f4b834f3bc884df63c39e05adf6dce8cb8
brijesh007 <uietbrijesh@gmail.com>
2024-10-21 16:06:03 +0530

    starter

---
+++ src/components/Body.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from './Login';
+import Browse from './Browse';
+
+const Body = () => {
+  return (
+    <div>
+      <Login></Login>
+      <Browse></Browse>
+    </div>
+  )
+}
+
+export default Body;
